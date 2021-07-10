4c37346e86b3541098c67ee88c6e041c8faa414b
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2021-07-10 18:53:01 -0300

    Update analise_reuniao_ppa.py

--- Atividade_1/analise_reuniao_ppa.py
+++ Atividade_1/analise_reuniao_ppa.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.ticker as ticker
+
 
 #lendo excel sem queries
 ba = pd.ExcelFile('C:/Users/Usuário/Desktop/quantities/Indicadores para pente fino_29-06-2021.xlsx')
@@ -47,7 +50,47 @@ ppa_ind=ppa_ind.fillna({'Sugestão (manter/alterar/substituir)': 'Sem Sugestão'
 
 # count plot on single categorical variable
 geral=sns.countplot(x ='Sugestão (manter/alterar/substituir)', data = ppa_ind)
-geral.set(xlabel='Sugestão', ylabel='',title='Sugestão')
+geral.set(xlabel='Sugestão', ylabel='',title='Situação dos indicadores revisados do PPA 2020-2023')
  
 # Show the plot
+# Some random data
+ncount = len(ppa_ind)
+
+# Make twin axis
+#ax2=geral.twinx()
+
+# Switch so count axis is on right, frequency on left
+#ax2.yaxis.tick_left()
+#geral.yaxis.tick_right()
+
+# Also switch the labels over
+geral.yaxis.set_label_position('right')
+#ax2.yaxis.set_label_position('left')
+
+#ax2.set_ylabel('[%]')
+
+for p in geral.patches:
+    x=p.get_bbox().get_points()[:,0]
+    y=p.get_bbox().get_points()[1,1]
+    geral.annotate('{:.1f}%'.format(100.*y/ncount), (x.mean(), y), 
+            ha='center', va='bottom') # set the alignment of the text
+
+# Fix the frequency range to 0-100
+#ax2.set_ylim(0,100)
+#geral.set_ylim(0,ncount)
+
+# And use a MultipleLocator to ensure a tick spacing of 10
+#ax2.yaxis.set_major_locator(ticker.MultipleLocator(10))
+
+# Need to turn the grid on ax2 off, otherwise the gridlines end up on top of the bars
+#ax2.grid(None)
+
+for p in geral.patches:
+    geral.annotate(f'\n{p.get_height()}', (p.get_x()+0.25, p.get_height()+1), ha='center', va='top', color='white', size=18)
+
+#geral.set_size_inches( 16, 10)
+geral.fig.set_size_inches(15,15)
+sns.despine(geral,left=True)
+
+
 plt.show(geral)
