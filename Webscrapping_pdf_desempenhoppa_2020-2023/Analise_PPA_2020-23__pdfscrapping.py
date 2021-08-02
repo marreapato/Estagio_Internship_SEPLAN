24b73546efc81373835be0815641edd6c10eadca
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2021-08-02 13:43:15 -0300

    Update Analise_PPA_2020-23__pdfscrapping.py

--- Webscrapping_pdf_desempenhoppa_2020-2023/Analise_PPA_2020-23__pdfscrapping.py
+++ Webscrapping_pdf_desempenhoppa_2020-2023/Analise_PPA_2020-23__pdfscrapping.py
@@ -14,3 +14,7 @@ import os
 
 #Tabela da página 20 do livro: http://www.seplan.ba.gov.br/arquivos/File/avaliacao_do_PPA/2020/relatorio_adp_v_final_ano_1_30042021_1042.pdf
 table = tabula.read_pdf('relatorio_adp_v_final_ano_1_30042021_1042.pdf', pages="20")
+
+table=table[0]
+
+table.iloc[6,0]='306 – Educação'
