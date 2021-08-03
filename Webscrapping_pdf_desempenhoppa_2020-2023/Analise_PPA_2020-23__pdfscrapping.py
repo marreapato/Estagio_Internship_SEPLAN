10d36bf0ae39c6808a2cc6d70ee3763df9292408
Lucas Rabelo <50274278+marreapato@users.noreply.github.com>
2021-08-03 17:49:39 +0000

    Update Analise_PPA_2020-23__pdfscrapping.py

--- Webscrapping_pdf_desempenhoppa_2020-2023/Analise_PPA_2020-23__pdfscrapping.py
+++ Webscrapping_pdf_desempenhoppa_2020-2023/Analise_PPA_2020-23__pdfscrapping.py
@@ -104,5 +104,49 @@ table2.iloc[:,1]=table2.iloc[:,1].str.replace(',','.')
 
 table2.iloc[:,1]=pd.to_numeric(table2.iloc[:,1], errors='coerce')
 table2.describe()
-
+table2.columns
 #media do do indicador de execução orcamentaria é de 42.72% ou seja, em média os programas tiveram uma execução financeira regular
+
+##########################################################
+###########################################
+########################
+###########
+######
+###
+##
+#
+#Barplo
+ax=sns.countplot(table2["Classificação da Execução"],palette="Set3")  
+plt.ylabel('Quantidade')
+
+# Make twin axis
+ax2=ax.twinx()
+
+# Switch so count axis is on right, frequency on left
+ax2.yaxis.tick_left()
+ax.yaxis.tick_right()
+
+# Also switch the labels over
+ax.yaxis.set_label_position('right')
+ax2.yaxis.set_label_position('left')
+
+ax2.set_ylabel('Frequência [%]')
+ncount=16
+for p in ax.patches:
+    x=p.get_bbox().get_points()[:,0]
+    y=p.get_bbox().get_points()[1,1]
+    ax.annotate('{:.1f}%'.format(100.*y/ncount), (x.mean(), y), 
+            ha='center', va='bottom') # set the alignment of the text
+
+# Use a LinearLocator to ensure the correct number of ticks
+ax.yaxis.set_major_locator(ticker.LinearLocator(11))
+
+# Fix the frequency range to 0-100
+ax2.set_ylim(0,100)
+ax.set_ylim(0,ncount)
+
+# And use a MultipleLocator to ensure a tick spacing of 10
+ax2.yaxis.set_major_locator(ticker.MultipleLocator(10))
+
+# Need to turn the grid on ax2 off, otherwise the gridlines end up on top of the bars
+ax2.grid(None)
